78d72de94ca575366c5f70f3f7e2bf3b85f93af8
Kostiantyn Khomenko <kostiantyn_khomenko@epam.com>
2014-05-12 23:18:09 +0300

    Adding StepDefinitionBase and examples

---
+++ NUnit.Selenium/BaseStepDefinitionExamples/SearchForTDDSteps.cs
@@ -0,0 +1,33 @@
+﻿using Specflow.FluentAutomation.Ext;
+using TechTalk.SpecFlow;
+
+namespace NUnit.Selenium
+{
+	[Binding]
+	public class SearchForTDDSteps : StepDefinitionBase
+	{
+		[Given(@"I have entered bing\.com")]
+		public void GivenIHaveEnteredBing_Com()
+		{
+			I.Open("http://bing.com");
+		}
+
+		[When(@"I enter TDD to the search field")]
+		public void WhenIEnterTDDToTheSearchField()
+		{
+			I.Enter("TDD").In("#sb_form_q");
+		}
+
+		[When(@"I press enter")]
+		public void WhenIPressEnter()
+		{
+			I.Press("{ENTER}");
+		}
+
+		[Then(@"the first link should be wiki")]
+		public void ThenTheFirstLinkShouldBeWiki()
+		{
+			I.Assert.Exists("a[href='http://en.wikipedia.org/wiki/Test-driven_development']");
+		}
+	}
+}
---
+++ Specflow.FluentAutomation.Ext/StepDefinitionBase.cs
@@ -0,0 +1,29 @@
+﻿using FluentAutomation;
+using FluentAutomation.Interfaces;
+using TechTalk.SpecFlow;
+
+namespace Specflow.FluentAutomation.Ext
+{
+	public abstract class StepDefinitionBase
+	{
+		protected FluentTest CurrentTest
+		{
+			get { return (FluentTest) ScenarioContext.Current[ScenarioContext.Current.ScenarioInfo.Title]; }
+		}
+
+		protected INativeActionSyntaxProvider I
+		{
+			get { return CurrentTest.I; }
+		}
+
+		protected object Provider
+		{
+			get { return CurrentTest.Provider; }
+		}
+
+		protected FluentSession Session
+		{
+			get { return CurrentTest.Session; }
+		}
+	}
+}
